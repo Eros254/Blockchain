4220687f6ba27a9a02475c06d656b273b4415a3b
Erchad Co <114959023+Eros254@users.noreply.github.com>
2025-01-15 20:30:30 +0300

    Update learn.sol

--- contracts/learn.sol
+++ contracts/learn.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+contract SimpleStorage{
+
+    //will get intialized to zero
+    uint256  favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+}
